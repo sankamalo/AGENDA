46de9b8dc00511fbda1de9a23e85d24066fbfe7f
sankalibrado <115594169+sankamalo@users.noreply.github.com>
2026-07-09 23:19:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-v15';
+const CACHE = 'agenda-v16';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
